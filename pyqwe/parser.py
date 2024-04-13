411c31390f7c9591cf06926db6986004e48e2583
David Carmichael <carmichaelits@gmail.com>
2024-04-13 13:50:22 +0100

    style: black

--- pyqwe/parser.py
+++ pyqwe/parser.py
@@ -10,13 +10,14 @@ class ArgumentParser(argparse.ArgumentParser):
         self.options = []
 
     def print_help(self, file=None):
-        print("\n\r"
-              "Usage: pyqwe <option> "
-              "\n\r\n\r"
-              f" {Colr.OKCYAN}list, ls{Colr.END} => List all commands found in pyproject.toml"
-              "\n\r"
-              f" {Colr.OKCYAN}-h, --help{Colr.END} => Show the help message and exit"
-              "\n\r"
-              f" {Colr.OKCYAN}-v, --version{Colr.END} => Show the version and exit"
-              )
+        print(
+            "\n\r"
+            "Usage: pyqwe <option> "
+            "\n\r\n\r"
+            f" {Colr.OKCYAN}list, ls{Colr.END} => List all commands found in pyproject.toml"
+            "\n\r"
+            f" {Colr.OKCYAN}-h, --help{Colr.END} => Show the help message and exit"
+            "\n\r"
+            f" {Colr.OKCYAN}-v, --version{Colr.END} => Show the version and exit"
+        )
         print("")
