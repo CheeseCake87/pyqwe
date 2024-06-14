84f247b86c76936f1812b0bb1934b121bdb688c9
David Carmichael <carmichaelits@gmail.com>
2024-06-14 15:53:01 +0100

    chore: version increase

--- pyqwe/__init__.py
+++ pyqwe/__init__.py
@@ -12,7 +12,7 @@ except ImportError:
     except ImportError:
         raise ImportError("pyqwe requires toml, install it with 'pip install toml'")
 
-__version__ = "1.5.1"
+__version__ = "1.5.2"
 
 _cwd = Path().cwd()
 _pyproject_file = _cwd / "pyproject.toml"
